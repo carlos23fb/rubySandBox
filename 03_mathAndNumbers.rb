1ab5712d171f453fa74c58d7947d59e4a7b6af02
carlos23fb <jcflobe@gmail.com>
2021-02-17 19:23:52 -0700

    first commit

---
+++ 03_mathAndNumbers.rb
@@ -0,0 +1,11 @@
+# puts "hens #{25 + 30 / 6}"
+# puts "Roosters #{10 % 2}"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
